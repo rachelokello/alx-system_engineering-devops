9755caabd243fb4ca7e4274233446c61e638b093
rachelokello <okelloawuor@gmail.com>
2022-08-02 06:48:30 -0700

    correcting regex

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
+# simply match school
+
 puts ARGV[0].scan(/School/).join
--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,2 +1,4 @@
 #!/usr/bin/env ruby
+#accepts one argument and pass it to a regular exprseeion matching method
+
 puts ARGV[0].scan(/^h.n$/).join
